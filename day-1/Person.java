e82ab3a678f9dceba0889f202ad69e035c089a4c
Si Thu San <sithusanss@gmail.com>
2022-01-07 21:55:03 +0630

    Creating Objects from a class

--- day-1/Person.java
+++ day-1/Person.java
@@ -1,11 +1,21 @@
 public class Person {
     public static void main(String[] args) {
-        System.out.println("----------------");
-        System.out.println("---Hello world---");
-        System.out.println("----------------");
 
-        for (String s : args) {
-            System.out.println(s);
-        }
+        Student student = new Student();
+        student.name = "Si Thu San";
+        student.age = 20;
+
+        student.greet();
+    }
+}
+
+// need to separate
+class Student {
+    String name;
+    int age;
+
+    void greet() {
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
     }
 }
