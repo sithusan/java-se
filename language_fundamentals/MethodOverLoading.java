c6b6669d1be9e7772da1610d0ea564d982966fb7
Si Thu San <sithusanss@gmail.com>
2022-01-09 16:09:50 +0630

    Method Overloading

---
+++ language_fundamentals/MethodOverLoading.java
@@ -0,0 +1,25 @@
+public class MethodOverLoading {
+    public static void main(String[] args) {
+        Paper paper = new Paper();
+        NoteBook noteBook = new NoteBook();
+
+        takeNote("Hello Java", paper);
+        System.out.println(paper.message);
+    }
+
+    static void takeNote(String message, Paper paper) {
+        paper.message = message;
+    }
+
+    static void takeNote(String message, NoteBook noteBook) {
+        noteBook.message = message;
+    }
+}
+
+class Paper {
+    String message;
+}
+
+class NoteBook {
+    String message;
+}
