a4db4e7f85d200b6fce69afeb35ce0feac33279e
Si Thu San <sithusanss@gmail.com>
2022-01-18 22:55:41 +0630

    Reference Types

---
+++ language_fundamentals/ReferenceType.java
@@ -0,0 +1,11 @@
+package language_fundamentals;
+
+public class ReferenceType {
+    public static void main(String[] args) {
+
+        ReferenceType referenceType = new ReferenceType();
+        String str = referenceType.toString();
+
+        System.out.println(str);
+    }
+}
