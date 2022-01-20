ae53ee200314482a945829b00285aaf46376c797
Si Thu San <sithusanss@gmail.com>
2022-01-20 23:25:57 +0630

    Writing Arrays

---
+++ language_fundamentals/WritingArrays.java
@@ -0,0 +1,15 @@
+package language_fundamentals;
+
+public class WritingArrays {
+    public static void main(String[] args) {
+
+        int array[] = { 1, 2, 3, 4, 5 };
+        String strArray[] = { "Hello", "Java" };
+
+        int[][] mutlti = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        System.out.println(array[0]);
+        System.out.println(strArray[0]);
+        System.out.println(mutlti[0][0]);
+    }
+}
